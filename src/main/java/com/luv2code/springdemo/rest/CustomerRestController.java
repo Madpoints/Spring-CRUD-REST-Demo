ecc3606df68ae181e218e1c2d4fbcf150131a293
John Furtado <jf8846@gmail.com>
2018-09-25 19:37:45 -0400

    delete customer with REST api

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,6 +56,22 @@ public class CustomerRestController {
 		customerService.saveCustomer(theCustomer);
 		
 		return theCustomer;
+	}
+	
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		
+		Customer tempCustomer = customerService.getCustomer(customerId);
+		
+		// throw exception 
+		if (tempCustomer == null) {
+			throw new CustomerNotFoundException("Customer with id - " + customerId +
+												" not found");
+		}
+		
+		customerService.deleteCustomer(customerId);
+		
+		return "Deleted customer with id - " + customerId;
 	}
 	
 }
