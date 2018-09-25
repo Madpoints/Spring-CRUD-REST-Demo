0a3731c923fdf64b1ed61561acaad7729b857d61
John Furtado <jf8846@gmail.com>
2018-09-25 18:39:42 -0400

    update customer with REST api

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +44,14 @@ public class CustomerRestController {
 		
 		// set id to 0 to force save of new item instead of update
 		theCustomer.setId(0);
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
+		
 		customerService.saveCustomer(theCustomer);
 		
 		return theCustomer;
